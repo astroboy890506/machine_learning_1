62f2f58e5fef1d05ed4bb8b3e0e0f717c9a643f1
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-14 21:31:24 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import pandas as pd
-from test1 import load_model
+import joblib
 
-model = load_model()
+# Load the trained machine learning model
+model = joblib.load('car_price_model.pkl')
+
+# Streamlit app
+st.title('Car Price Prediction')
+
+# Sidebar inputs
+st.sidebar.header('Input Features')
+horsepower = st.sidebar.slider('Horsepower', 50, 300, 150)
+weight = st.sidebar.slider('Weight', 1500, 5000, 3000)
+
+# User input data
+input_data = pd.DataFrame({'horsepower': [horsepower], 'weight': [weight]})
+
+# Make a prediction
+prediction = model.predict(input_data)[0]
+
+# Display prediction
+st.write(f'Predicted Price: ${prediction:.2f}')
