904d3630144f505cd8d86a8e27f12b9336d24ed7
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-15 20:04:08 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -28,3 +28,23 @@ test_score = model.score(X_test, y_test)
 print(f"Training R2 Score: {train_score:.2f}")
 print(f"Testing R2 Score: {test_score:.2f}")
 joblib.dump(model, 'car_price_model.pkl')
+
+# Load the trained machine learning model
+model = joblib.load('car_price_model.pkl')
+
+# Streamlit app
+st.title('Car Price Prediction')
+
+# Sidebar inputs
+st.sidebar.header('Input Features')
+horsepower = st.sidebar.slider('Horsepower', 50, 300, 150)
+weight = st.sidebar.slider('Weight', 1500, 5000, 3000)
+
+# User input data
+input_data = pd.DataFrame({'horsepower': [horsepower], 'weight': [weight]})
+
+# Make a prediction
+prediction = model.predict(input_data)[0]
+
+# Display prediction
+st.write(f'Predicted Price: ${prediction:.2f}')
